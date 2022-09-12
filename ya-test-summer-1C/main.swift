9b9c7bcf88d77b7219efe8c3c76c9c68174ad0dd
avedevios <106875633+avedevios@users.noreply.github.com>
2022-09-12 15:35:27 +0300

    Best solution

--- ya-test-summer-1C/main.swift
+++ ya-test-summer-1C/main.swift
@@ -7,64 +7,43 @@
 
 import Foundation
 
-var n = 6
-var arr = [10, 3, 5, 3, 11, 9]
-//n = 4
-//arr = [5,5,5,5]
-//n = 2
-//arr = [5,-10]
+func Super(n: Int, arr: [Int]) -> (Int, Int) {
+    var minCostDay = 0
+    var bestBuyDay = 0
+    var bestSellDay = 0
+    
+    for j in 1...n - 1 {
+        if arr[bestSellDay] * arr[minCostDay] < arr[bestBuyDay] * arr[j] {
+            bestBuyDay = minCostDay
+            bestSellDay = j
+        }
+        if arr[j] < arr[minCostDay] {
+            minCostDay = j
+        }
+    }
+    if bestSellDay == 0 && bestBuyDay == 0 {
+        return (0,0)
+    } else {
+        return (bestBuyDay + 1, bestSellDay + 1)
+    }
+}
 
-//let nString = readLine()!
-//let aString = readLine()!
-//
-//let n = Int(nString)!
-//
-//var arr = [Int]()
-//let a = aString.split(separator: " ")
-//for element in a {
-//    arr.append(Int(element)!)
-//}
+let nString = readLine()!
+let aString = readLine()!
+
+let n = Int(nString)!
+
+var arr = [Int]()
+let a = aString.split(separator: " ")
+for element in a {
+    arr.append(Int(element)!)
+}
+
 let cash = 1000.0
 var res = (0, 0)
 
-func Slow(n: Int, arr: [Int]) -> (Int, Int) {
-    var max = 0.0
-    var ans = (0,0)
-    var gasVolume = 0.0
-    for i in 0...n - 2 {
-        gasVolume = cash / Double(arr[i])
-        for j in i + 1...n - 1 {
-            if max < gasVolume * Double(arr[j]) - cash {
-                max = gasVolume * Double(arr[j]) - cash
-                ans = (i + 1, j + 1)
-            }
-        }
-    }
-    return ans
-}
-
-func Fast(n: Int, arr: [Int]) -> (Int, Int) {
-    var iMin = 0
-    var gasVolume = cash / Double(arr[0])
-    var max = 0.0
-    var ans = (0,0)
-    
-    for j in 1...n - 1 {
-        let price = Double(arr[j])
-        if max < gasVolume * price - cash {
-            max = gasVolume * price - cash
-            ans = (iMin + 1, j + 1)
-        }
-        if gasVolume < cash / price {
-            iMin = j
-            gasVolume = cash / price
-        }
-    }
-    return ans
-}
-
 if n > 1 {
-    res = Fast(n: n, arr: arr)
+    res = Super(n: n, arr: arr)
 }
 
 print (res.0)
